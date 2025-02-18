f5cd23bc83c752668b06c2c4f6c24ab14691434b
aves94 <anjansen30@gmail.com>
2025-02-18 13:13:43 +0530

    MovieApp is an Angular-based web application

---
+++ src/app/feature/star-rating/star-rating.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  templateUrl: './star-rating.component.html',
+  styleUrls: ['./star-rating.component.scss']
+})
+export class StarRatingComponent {
+
+  @Input() rating: any;
+  @Input() isReadOnly: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+}
